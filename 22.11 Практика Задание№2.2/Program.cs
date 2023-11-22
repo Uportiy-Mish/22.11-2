a3216d4b7aab13f41622cd2d4b72b4d060adc52d
Asus <Asus@DESKTOP-Q1VVDVK>
2023-11-22 12:15:15 +0300

    Добавьте файлы проекта.

---
+++ 22.11 Практика Задание№2.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _22._11_Практика_Задание_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Interaction.InputBox("Введите ваше имя",
+                "Ввод имени");
+            string age;
+            age = Interaction.InputBox("Введите ваш возраст",
+                "Ввод возраста");
+            string txt = "Имя: " + name + ", Возраст: " + age;
+            MessageBox.Show(txt, "Итог:");
+
+        }
+    }
+}
---
+++ 22.11 Практика Задание№2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11_Практика_Задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Имя");
+            string name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Введите Возраст");
+            string age = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Имя: {name}, Возраст: {age}");
+            Console.ReadKey();
+
+        }
+    }
+}
